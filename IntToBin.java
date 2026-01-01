a8f7fc8cc66eb748efad67759368807f324405f8
Yair efron <yaire@MacBook-Air-sl-Yair.local>
2026-01-01 18:01:57 +0200

    first try to binary

--- IntToBin.java
+++ IntToBin.java
@@ -8,7 +8,24 @@ public class IntToBin {
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
+        if(x==1)
+        {
+            return "1";
+        }
+        if (x==0) 
+        {
+            return "0";
+        }
+        else
+        {
+            if ((x % 2) == 0) 
+            {
+            return toBinary(x/2)+"0";
+            }
+            else 
+            {
+            return toBinary(x/2)+"1";
+            }
+        }
     }    
- }
+}
