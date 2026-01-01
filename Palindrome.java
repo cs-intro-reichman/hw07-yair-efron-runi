f3713c4bf3fd7b2b8bc26b3582e49084b71c10dc
Yair efron <yaire@MacBook-Air-sl-Yair.local>
2026-01-01 18:37:05 +0200

    first isplaindrome

--- Palindrome.java
+++ Palindrome.java
@@ -6,8 +6,14 @@ public class Palindrome {
     }
 	
 	/** Checks if the given string is a palindrome. */
-	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
-    }
+	public static boolean isPalindrome(String s) 
+	{
+		if (s.length()==0) {
+			return true;
+		}
+		if (s.charAt(0) != s.charAt(s.length()-1)) {
+			return false;
+		}
+		return true&& isPalindrome(s.substring(1, s.length()-2));
+  }
 }
