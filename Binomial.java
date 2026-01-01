d3075af5622d2fb8eada136af0883bb671aba820
Yair efron <yaire@MacBook-Air-sl-Yair.local>
2026-01-01 19:33:55 +0200

    this and that

--- Binomial.java
+++ Binomial.java
@@ -12,24 +12,30 @@ public class Binomial {
 
 	// Computes the Binomial function, basic version.
 	public static int binomial1(int n, int k) { 
-		//// Repplace the following comment with your code
-		return 0;
+		if (k > n) return 0;
+		if (k == 0 || n == 0) return 1;
+		return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
 	 }
-	
+	 
 	// Computes the Binomial function, efficiently
 	public static int binomial(int n, int k) {
-		//// This function creates a 2D array, say memo, 
-		//// and then initializes all its elements to -1.
-		//// It then calls binomial(n, k, memo), which does all the heavy lifiting.
-		//// Replace the following statement with your code.
-		return 0;
+		if (k > n) return 0;
+        if (k == 0 || n == 0) {return 1;}
+        int[][] memo = new int[n + 1][k + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
+        return binomial(n, k, memo);
 	}
 
 	private static int binomial(int n, int k, int[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
-		// Base case
+		// Base caseאבל
 		if ((k > n)) {
 		   	memo[n][k] = 0; 
 		   	return 0;
